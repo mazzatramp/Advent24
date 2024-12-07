9fc9681402cdd915f20838e2467ac31d23e2ae65
Filippo Mazzarotto <37803729+mazzatramp@users.noreply.github.com>
2024-12-07 16:30:53 +0100

    Refactor day2

--- 2024-12-02/day2.cpp
+++ 2024-12-02/day2.cpp
@@ -6,89 +6,103 @@
 #include <sstream>
 #include <map>
 
-std::pair<int,int> getNears(std::vector<int>& report, int i, int ig=-1) {
-    int first = i == ig ? i+1 : i; 
-    int second = (i == ig || i+1 == ig) ? i+2 : i+1;
-
-    if (first < report.size() && second < report.size()) {
-        return {report[first], report[second]};
-    }
-    else return {report[0], report[1]}; //return something that would pass tests.
+int shiftIndexOnThreshold(int i, int th) {
+    if ( i >= th) return i+1;
+    else return i;
 }
 
-bool stillMonotone(bool increasing, std::pair<int,int> nears) {
+bool stillMonotone(bool increasing, const std::pair<int,int> nears) {
     return increasing && nears.first < nears.second
             || !increasing && nears.first > nears.second;
 }
 
-bool smallDifference(std::pair<int,int> nears) {
-    int diff = std::abs(nears.first - nears.second);
+bool acceptableDiff(int a, int b) {
+    int diff = std::abs(a - b);
     return diff >= 1 && diff <= 3;
 }
 
-bool isSafe (std::vector<int> report) {
-    bool increasing = report.front() < report.back();
-    bool safe_report = true;
-    std::vector<int> toIgnore = {};
-
-    static int c = 1;
-    //std::cout << c++ << ": \t";
-    for (int i = 0; i < report.size() - 1; ++i) {
-        std::pair<int,int> nears = getNears(report, i);
-        
-        if (!stillMonotone(increasing, nears) || !smallDifference(nears)) { 
-            safe_report = false;
-            toIgnore.push_back(i);
-            toIgnore.push_back(i+1);
-            //std::cout << i << "," << i+1 << " ";
-        }
-    }
-
-    for(int j = 0; j < toIgnore.size() && !safe_report; ++j) {
-        safe_report = true;
-        std::pair<int,int> initials = getNears(report, 0, toIgnore[j]);
-        increasing = initials.first < initials.second;
-        for (int i = 0; i < report.size() - 1 && safe_report; ++i) {
-            std::pair<int,int> nears = getNears(report, i, toIgnore[j]);
-            safe_report = stillMonotone(increasing, nears) && smallDifference(nears);
-        }
-    }
-
-    return safe_report;
+int monotony(int a, int b) {
+  if (a == b) {
+    return 0;  // No change
+  } else if (b > a) {
+    return 1;  // Increasing
+  } else {
+    return -1; // Decreasing
+  }
 }
 
-int main() {
-    std::ifstream file("input.txt");
-    std::string line;
+bool sameMonotony(int a1, int b1, int a2, int b2) {
+    return monotony(a1,b1) == monotony(a2,b2);
+}
+
+std::vector<int> getErrorIndexes(const std::vector<int>& r) {
+    std::vector<int> errorIndexes = {};
+
+    for (size_t i = 0; i < r.size() - 2; ++i) {
+        if (!sameMonotony(r[0], r[1], r[i], r[i+1]) || !acceptableDiff(r[i], r[i+1])) { 
+            errorIndexes.push_back(i);
+            errorIndexes.push_back(i+1);
+        }
+    }
+    return errorIndexes;
+}
+
+bool isSafeIgnoring(const std::vector<int> r, size_t i_ignore) {
+    if (r.size() <= 2)
+        return true;
+    
+    if (i_ignore == r.size() -1 ) return false;
+
+    for (size_t i = 0, j = 1; j < r.size() - 1; ++i, ++j) {
+        if (i == i_ignore) {j++; continue;};
+        if (j == i_ignore) j++;
+
+        if (!sameMonotony(r[0], r[1], r[i], r[j]) || !acceptableDiff(r[i], r[j])) { 
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isSafe(const std::vector<int> report) {
+    std::vector<int> errIndexes = getErrorIndexes(report);
+    for(auto& error : errIndexes) {
+        if (isSafeIgnoring(report, error))
+            return true;
+    }
+    return errIndexes.empty();
+}
+
+std::vector<int> splitBySpace(std::string toSplit) {
+    std::vector<int> values = {};
+    std::stringstream ss{toSplit};
+    int value;
+    while(ss >> value)
+        values.push_back(value);
+    return values;
+}
+
+std::vector<std::vector<int>> extractReports(std::string& filename) {
     std::vector<std::vector<int>> reports;
+    std::ifstream file{filename};
+    std::string line;
 
     while(std::getline(file, line)) {
-        std::stringstream ss(line);
-        reports.push_back({});
-        int num;
-        while(ss >> num) {
-            reports.back().push_back(num);
-        }
+        reports.push_back(splitBySpace(line));
     }
+    return reports;
+};
 
+int countSafeReports(std::vector<std::vector<int>>& reports) {
     int safe_count = 0;
-    int count = 1;
-    for (auto& report : reports) {
-        bool issafeee = isSafe(report);
-        safe_count += issafeee;
+    for (auto& report : reports)
+        safe_count += isSafe(report);
+    return safe_count;
+}
 
-
-
-        if (!issafeee) {
-            for(int& n : report) {
-               //std::cout << n << " ";
-            }
-            //std::cout << "\tunsafe";
-        }
-        //std::cout << std::endl;
-    }
-
-    std::cout << std::endl << safe_count << std::endl;
-
+int main(int argc, char* argv[]) {
+    std::string report_filename = (argc == 2) ? argv[1]:  "input.txt";
+    std::vector<std::vector<int>> reports = extractReports(report_filename);
+    std::cout << countSafeReports(reports) << std::endl;
     return 0;
 }
